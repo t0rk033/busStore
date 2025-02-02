69c8c4aea9882248fa300c4e3048b6302ff55b1a
t0rk033 <thiagocisse42@gmail.com>
2025-02-02 07:22:25 -0300

    Corrigindo caminho de arquivos no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/busStore/", // Defina como o nome do seu repositório
+  base: "/busstoreteste/", // Caminho do subdiretório no domínio
 });
