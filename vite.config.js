11c8a23027daef51ff0ee28722a3863e1fb09f10
t0rk033 <thiagocisse42@gmail.com>
2025-02-02 07:29:24 -0300

    Configuração para domínio personalizado

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/busstoreteste/", // Caminho do subdiretório no domínio
+  base: "/", // Define a raiz do domínio como base
 });
